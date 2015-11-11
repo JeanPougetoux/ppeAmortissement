d3001453b03c0801be0271e13e11432244a3f5ce
thomas <thomasecalle@hotmail.fr>
2015-11-11 18:05:34 +0100

    classe Crédit presque fini

--- ppeAmortissement/src/amortissements/Credit.java
+++ ppeAmortissement/src/amortissements/Credit.java
@@ -64,8 +64,7 @@ public class Credit
 	
 	public int duree()
 	{
-		// TODO à compléter
-		return 0;
+		return duree;
 	}
 	
 	/**
@@ -86,9 +85,45 @@ public class Credit
 			double montantEmprunte, double annuiteMaximale,
 			int duree)
 	{
-		// TODO à compléter
+		
+		if (typeCredit == 1){
+			double amortissement = montantEmprunte/duree;
+			double interet = (annuiteMaximale - amortissement);
+			double taux = interet/montantEmprunte;
+			return new Credit(typeCredit, montantEmprunte, annuiteMaximale, taux, duree);
+			
+		}
+		else if (typeCredit == 2){
+			double annuite = annuiteMaximale +1;
+			double tauxProbable = 1;
+			double tauxMax = tauxProbable;
+			double tauxMin;
+
+			while (annuite != annuiteMaximale){
+				
+				annuite = AnnuiteCalculeTaux(montantEmprunte,duree,tauxProbable);
+				if (annuite > annuiteMaximale){
+					tauxMax = tauxProbable;
+					tauxProbable = tauxProbable/2;
+					
+				}
+				else if (annuite < annuiteMaximale){
+					tauxMin = tauxProbable;
+					tauxProbable = tauxProbable + ((tauxMax - tauxProbable)/2);
+					
+				}
+			}
+			return new Credit(typeCredit, montantEmprunte, annuiteMaximale, tauxProbable, duree);
+		
+			
+		}
 		return null;
 	}
+	public static double AnnuiteCalculeTaux(double montant, int duree, double taux){
+		double annuite = (montant*taux)/(1-Math.pow(1+taux, -duree));
+		return annuite;
+	}
+
 	
 	/**
 	 * Retourne un crédit en calculant automatiquement la durée.
@@ -98,9 +133,22 @@ public class Credit
 			double montantEmprunte, double annuiteMaximale,
 			double taux)
 	{
-		// TODO à compléter
+		if(typeCredit == 1){
+			int duree = (int)(montantEmprunte/(annuiteMaximale - montantEmprunte*taux));
+			Credit cred = new Credit(typeCredit, montantEmprunte, annuiteMaximale, taux, duree);
+			return cred;
+		}
+		else if (typeCredit == 2){
+			int duree = 0;
+			while(montantEmprunte>0){
+				montantEmprunte = montantEmprunte - (annuiteMaximale - montantEmprunte * taux);
+				duree ++;
+			}
+			return new Credit(typeCredit, montantEmprunte, annuiteMaximale, taux, duree);
+		}
 		return null;
 	}
+	
 
 	/**
 	 * Retourne un crédit en calculant automatiquement le montant
@@ -110,7 +158,14 @@ public class Credit
 	public static Credit calculeMontantEmprunte(int typeCredit, 
 			double annuiteMaximale,	double taux, int duree)
 	{
-		// TODO à compléter
+		if (typeCredit == 1){
+			double montantEmprunte = annuiteMaximale / (taux + 1/(double)(duree));
+			return new Credit (typeCredit, montantEmprunte, annuiteMaximale, taux, duree);
+		}
+		else if (typeCredit == 2){
+			double montantEmprunte = (annuiteMaximale*(1-Math.pow(1+taux, -duree)))/(taux);
+			return new Credit (typeCredit, montantEmprunte, annuiteMaximale, taux, duree);
+		}
 		return null;
 	}
 
@@ -119,10 +174,29 @@ public class Credit
 	 * l'annuité maximale.
 	 */
 	
-	public static Credit calculeAnuiteMaximale(int typeCredit, 
+	public static Credit calculeAnnuiteMaximale(int typeCredit, 
 			double montantEmprunte,	double taux, int duree)
 	{
-		// TODO à compléter
+		if (typeCredit == 1){
+			double annuiteMaximale = (montantEmprunte*taux)+(montantEmprunte / duree);
+			return new Credit (typeCredit, montantEmprunte, annuiteMaximale, taux, duree);
+		}
+		else if (typeCredit == 2){
+			double annuiteMaximale = montantEmprunte * taux / (1 - Math.pow(1+taux, -duree));
+			return new Credit (typeCredit, montantEmprunte, annuiteMaximale, taux, duree);
+		}
 		return null;
+	}
+	public static void main(String[] args){
+		/*Credit cred = calculeMontantEmprunte(2,22391.8773,0.06,7);
+		double montant = cred.montantEmprunte();
+		System.out.println("Le montant emprunte est de  : " + montant + "euros");
+		//double a = AnnuiteCalculeTaux(20000,4,0.05);
+		*/
+		Credit mega = calculeAnnuiteMaximale(2, 35000,	0.07, 5);
+		System.out.println(mega.annuiteMaximale());
+		
+		Credit omega = calculeTaux (2,35000,8536.17,5);
+		System.out.println("letaux du pret est de :"+omega.taux()*100+" %");
 	}
 }

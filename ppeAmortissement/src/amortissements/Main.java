e4f44938f718310c48c7909ea7e8cb2d0e5ac633
thomas <thomasecalle@hotmail.fr>
2015-11-16 21:50:37 +0100

    (effacement des requetes de la classe Main afin d'éviter les conflits)

--- ppeAmortissement/src/amortissements/Main.java
+++ ppeAmortissement/src/amortissements/Main.java
@@ -4,11 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-	Credit cred = Credit.calculeAnnuiteMaximale(2,98000, 0.05,4);
-	System.out.println(cred.getTableauAmortissement());
-	System.out.println(cred.duree());
-	Credit cred2 = Credit.calculeAnnuiteMaximale(1,150000, 0.05, 4);
-	System.out.println(cred2.getTableauAmortissement());
-	System.out.println(cred2.taux());
+	
 	}
 }

b94706da4bd9cfc983478c061754c6fc310f40b1
Jean <Jean@PcdeJean>
2015-11-16 12:53:06 +0100

    Integration des calcul liés à l'interface(erreur de résultat)

--- ppeAmortissement/src/amortissements/FenetrePrincipale.java
+++ ppeAmortissement/src/amortissements/FenetrePrincipale.java
@@ -10,13 +10,13 @@ public class FenetrePrincipale extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	private JTextField taux, duree, emprunt, remboursement;
-	private JLabel printTaux, printDuree, printEmprunt, printRemboursement, messageErreur;
+	private JLabel messageErreur;
 	private JLabel labelTaux, labelDuree, labelEmprunt, labelRemboursement, labelBottom;
-	private JPanel panel, panel2;
+	private JPanel panel, panel2, panelTxt;
 	private JButton bouton, bouton2;
 	private JScrollPane scroll;
 	private JTable tableau;
-	private GridBagConstraints gbc;
+	private JComboBox<String> choix;
 
 	/**
 	 * Page principale où se passe toutes les interactions
@@ -34,14 +34,18 @@ public class FenetrePrincipale extends JFrame {
 	
 	private void build(){
 		setTitle("Gestion de l'amortissement"); 
-		setSize(850,480); 
+		setSize(850,520); 
 		setLocationRelativeTo(null); 
 		setResizable(false); 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 		setContentPane(buildContentPaneHead());
 		getContentPane().add(buildContentPane());
+		getContentPane().add(buildContentPaneBot());
 	}
 	
+	/*
+	 * Définit le panel ou seront tous les éléments du Header
+	 */
 	private JPanel buildContentPaneHead(){
 		panel2 = new JPanel();
 		panel2.setLayout(new FlowLayout());
@@ -50,6 +54,15 @@ public class FenetrePrincipale extends JFrame {
 		panelHead();
 		return panel2;
 	}
+	
+	private JPanel buildContentPaneBot(){
+		panelTxt = new JPanel();
+		panelTxt.setLayout(new GridLayout(3,1));
+		panelTxt.setBackground(Color.white);
+		drawBottom();
+		panelBottom();
+		return panelTxt;
+	}
 	/**
 	 * Permet de remplir le JPanel avec les différents composants et
 	 * l'intègre au JFrame (page principale)
@@ -57,13 +70,10 @@ public class FenetrePrincipale extends JFrame {
 	
 	private JPanel buildContentPane(){		
 		panel = new JPanel();
-		panel.setLayout(new GridBagLayout());
+		panel.setLayout(new FlowLayout());
 		panel.setBackground(Color.white);
 		drawTableau();
-		drawBody();
-		drawBottom();
 		panelTableau();
-		panelBottom();
 		return panel;
 	}
 	
@@ -80,37 +90,23 @@ public class FenetrePrincipale extends JFrame {
 		labelRemboursement = new JLabel("Annuité maximale : ");		
 		remboursement = new JTextField(04);
 		
+		String[] choixCredit = { "Annuitées constantes", "Amortissement constant"};
+		choix = new JComboBox<String>(choixCredit);
+		choix.setSelectedIndex(1);
 		bouton = new JButton(new BoutonAction(this, "valider"));
 	}
 	
-	/*
-	 * Concatène les chaines en bas pour n'en créer qu'une seule
-	 */
-	
-	private void drawPanelBottom(){
-		labelBottom = new JLabel(printTaux.getText() + printDuree.getText() +
-				printEmprunt.getText() + printRemboursement.getText());
-	}
-	/*
-	 * Initialise ce qui est au milieu de l'application (labels)
-	 */
-	private void drawBody(){
-		printTaux = new JLabel("Taux : ");
-		printDuree = new JLabel(",  Durée : ");
-		printEmprunt = new JLabel(",  Montant emprunté : ");
-		printRemboursement = new JLabel(",  Montant de remboursement : ");
-
-		bouton2 = new JButton(new ExportExcel(this, "export"));
-	}
 	
 	/*
 	 * Initialise le message d'erreur en bas de l'application
 	 */
 	
 	private void drawBottom(){
+		 labelBottom = new JLabel("Taux :   Durée :   Montant emprunté :   Montant de remboursement : ");
+		 bouton2 = new JButton(new ExportExcel(this, "export"));
 		 messageErreur = new JLabel("Veuillez ne saisir que des valeurs unitaires");
 	     messageErreur.setForeground(Color.red);
-	     messageErreur.setVisible(false);
+//	     messageErreur.setVisible(false);
 	}
 	
 	/*
@@ -125,6 +121,7 @@ public class FenetrePrincipale extends JFrame {
 		panel2.add(emprunt);
 		panel2.add(labelRemboursement);
 		panel2.add(remboursement);
+		panel2.add(choix);
 		panel2.add(bouton);
 	}
 	
@@ -133,19 +130,8 @@ public class FenetrePrincipale extends JFrame {
 	 */
 	
 	private void panelTableau(){
-		gbc = new GridBagConstraints();
-		gbc.gridx = 0;
-		gbc.gridy = 2;
-		gbc.gridwidth = GridBagConstraints.REMAINDER;		
-		gbc.fill = GridBagConstraints.BOTH;
-		gbc.insets = new Insets(20, 0, 0, 0);
-		panel.add(scroll, gbc);
-		gbc = new GridBagConstraints();
-		gbc.gridwidth = GridBagConstraints.REMAINDER;	
-		gbc.insets = new Insets(10, 300, 10, 0);
-		gbc.gridx = 8;
-		gbc.gridy = 3;
-		panel.add(bouton2, gbc);
+
+		panel.add(scroll);
 	}
 	
 	/*
@@ -153,17 +139,12 @@ public class FenetrePrincipale extends JFrame {
 	 */
 	
 	private void panelBottom(){
-		gbc = new GridBagConstraints();
-		drawPanelBottom();
-		gbc.gridx = 0;
-		gbc.gridy = 4;
-		gbc.gridwidth = 8;
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-		panel.add(labelBottom, gbc);
-		gbc.gridx = 0;
-		gbc.gridy = 5;
-		gbc.gridwidth = 8;
-		panel.add(messageErreur, gbc);
+		JPanel p = new JPanel(); 
+		p.add(bouton2); 
+		p.setBackground(Color.white);
+		panelTxt.add(p);
+		panelTxt.add(labelBottom);
+		panelTxt.add(messageErreur);
 	}
 	/**
 	 * Créer un tableau, le remplit de valeurs et l'ajoute au JPanel
@@ -200,20 +181,8 @@ public class FenetrePrincipale extends JFrame {
 		return remboursement;
 	}
 	
-	public JLabel getModifTaux(){
-		return printTaux;
-	}
-	
-	public JLabel getModifDuree(){
-		return printDuree;
-	}
-	
-	public JLabel getModifEmprunt(){
-		return printEmprunt;
-	}
-	
-	public JLabel getModifRemboursement(){
-		return printRemboursement;
+	public JLabel getLabelBottom(){
+		return labelBottom;
 	}
 	
 	public JLabel getErreur(){
@@ -222,5 +191,9 @@ public class FenetrePrincipale extends JFrame {
 	
 	public JTable getTableau(){
 		return tableau;
+	}
+	
+	public JComboBox<String> getCombo(){
+		return choix;
 	}
 }
--- ppeAmortissement/src/amortissements/Main.java
+++ ppeAmortissement/src/amortissements/Main.java
@@ -4,10 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Credit cred = Credit.calculeTaux(2, 20000, 5640.24, 4);
+//		Credit cred = Credit.calculeTaux(2, 2000000, 5640.24, 6);
+//		System.out.println(cred.getTableauAmortissement());
+		Credit cred = Credit.calculeDuree(2, 100000, 10000, 2);
 		System.out.println(cred.getTableauAmortissement());
-		
-
 	}
-
 }

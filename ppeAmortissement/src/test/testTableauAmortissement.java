a43cc9afbf98c2bcd3a8c76dc70e00dc44280038
Jean <Jean@PcdeJean>
2015-11-20 12:13:03 +0100

    Fin des test unitaires de trois classes de base

--- ppeAmortissement/src/test/testTableauAmortissement.java
+++ ppeAmortissement/src/test/testTableauAmortissement.java
@@ -4,21 +4,31 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import amortissements.Credit;
+import amortissements.Ligne;
+import amortissements.TableauAmortissement;
+
 public class testTableauAmortissement {
+
+	double montant = 100000;
+	double annuiteMaximale = 10000;
+	double taux = 0.05;
+	Credit cred = Credit.calculeDuree(Credit.AMORTISSEMENT_CONSTANTS, montant, annuiteMaximale, taux);		
+	TableauAmortissement tab = new TableauAmortissement(cred);
 
 	@Test
 	public void testGetNbLignes() {
-		fail("Not yet implemented");
+		assertEquals(cred.duree(), 20, 0.01);
 	}
 
 	@Test
 	public void testGetLigne() {
-		fail("Not yet implemented");
+		Ligne li = tab.getLigne(1);
+		assertTrue(li.getAmortissements() == 5000);
+		assertTrue(li.getAnnee() == 1);
+		assertTrue(li.getAnnuite() == 9750.00);
+		assertTrue(li.getCapitalFinal() == 90000.00);
+		assertTrue(li.getCapitalInitial() == 95000.00);
+		assertTrue(li.getInterets() == 4750.00);
 	}
-
-	@Test
-	public void testToString() {
-		fail("Not yet implemented");
-	}
-
 }
